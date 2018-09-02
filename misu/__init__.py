051738a9659384b8cffc6b84342ada3ef0d4cb87
Christian Gross <christian.gross@mpq.mpg.de>
2018-09-02 13:58:25 +0200

    import into global namespace

--- misu/__init__.py
+++ misu/__init__.py
@@ -1,4 +1,7 @@
+# the core
 from misu.misulib import UnitNamespace, units_to_this_ns
+
+# the conversion helpers
 from misu.misulib import (
     k_val_from_c,
     c_val_from_k,
@@ -7,5 +10,8 @@ from misu.misulib import (
     c_val_from_f,
     f_val_from_c,
 )
+
+# the wrappers
+from misu.misulib import noquantity, calc_unitless, dimensions
 
 # import misu.physicalconstants as pc
