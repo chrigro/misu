a020fe1102eaf5769c5e82dfb4d06a701bc4cc38
Christian Gross <christian.gross@mpq.mpg.de>
2018-09-05 08:52:19 +0200

    removed QuantityNP

--- misu/engine.pyx
+++ misu/engine.pyx
@@ -23,27 +23,6 @@ class ESignatureAlreadyRegistered(Exception):
 
 
 cdef class Quantity
-cdef class QuantityNP
-
-
-ctypedef fused Quant:
-    Quantity
-    QuantityNP
-
-
-ctypedef fused Qnumber:
-    Quantity
-    QuantityNP
-    int
-    double
-    float
-
-
-ctypedef fused magtype:
-    int
-    double
-    float
-    np.ndarray
 
 
 cdef inline int isQuantity(var):
@@ -52,17 +31,10 @@ cdef inline int isQuantity(var):
     return isinstance(var, Quantity)
 
 
-cdef inline int isQuantityT(Quant var):
-    ''' SPECIALIZED VERSION
-    Checks whether var is an instance of type 'Quantity' or
-    'QuantityNP'. Returns True or False.'''
-    return isinstance(var, Quantity) or isinstance(var, QuantityNP)
-
-
 ctypedef double[7] uarray
 
 
-cdef inline void copyunits(Quant source, Quant dest, float power):
+cdef inline void copyunits(Quantity source, Quantity dest, float power):
     ''' Cython limitations require that both source and dest are the same
     type. '''
     cdef int i
@@ -90,7 +62,7 @@ cdef inline Quantity assertQuantity(x):
 cdef list symbols = ['m', 'kg', 's', 'A', 'K', 'cd', 'mol']
 
 
-cdef inline sameunits(Quant self, Quant other):
+cdef inline sameunits(Quantity self, Quantity other):
     cdef int i
     for i from 0 <= i < 7:
         if self.unit[i] != other.unit[i]:
@@ -253,7 +225,6 @@ cdef class Quantity:
             return self._magnitude
         else:
             return self._magnitudeNP
-        
 
     # ------ Pickling support ------
 
@@ -263,110 +234,7 @@ cdef class Quantity:
     def __setstate__(self, unit):
         self.setunit(unit)
 
-
-    # ------ Implement some numpy functions ------
-
-    def _check_dimensionless(self):
-            if self.unitCategory() != 'Dimensionless':
-                raise EIncompatibleUnits('Argument must be dimensionless.')
-
-    def _get_from_numpy(self, name):
-        cdef Quantity out = Quantity.__new__(Quantity, getattr(np, name)(self.magnitude))
-        return out
-
-    def sin(self):
-        self._check_dimensionless()
-        return self._get_from_numpy('sin')
-
-    def cos(self):
-        self._check_dimensionless()
-        return self._get_from_numpy('cos')
-
-    def tan(self):
-        self._check_dimensionless()
-        return self._get_from_numpy('tan')
-
-    def arcsin(self):
-        self._check_dimensionless()
-        return self._get_from_numpy('arcsin')
-
-    def arccos(self):
-        self._check_dimensionless()
-        return self._get_from_numpy('arccos')
-
-    def arctan(self):
-        self._check_dimensionless()
-        return self._get_from_numpy('arctan')
-
-    def degrees(self):
-        self._check_dimensionless()
-        return self._get_from_numpy('degrees')
-
-    def radians(self):
-        self._check_dimensionless()
-        return self._get_from_numpy('radians')
-
-    def deg2rad(self):
-        self._check_dimensionless()
-        return self._get_from_numpy('deg2rad')
-
-    def rad2deg(self):
-        self._check_dimensionless()
-        return self._get_from_numpy('rad2deg')
-
-    def sinh(self):
-        self._check_dimensionless()
-        return self._get_from_numpy('sinh')
-
-    def cosh(self):
-        self._check_dimensionless()
-        return self._get_from_numpy('cosh')
-
-    def tanh(self):
-        self._check_dimensionless()
-        return self._get_from_numpy('tanh')
-
-    def arcsinh(self):
-        self._check_dimensionless()
-        return self._get_from_numpy('arcsinh')
-
-    def arccosh(self):
-        self._check_dimensionless()
-        return self._get_from_numpy('arccosh')
-
-    def arctanh(self):
-        self._check_dimensionless()
-        return self._get_from_numpy('arctanh')
-
-    def exp(self):
-        self._check_dimensionless()
-        return self._get_from_numpy('exp')
-
-    def expm1(self):
-        self._check_dimensionless()
-        return self._get_from_numpy('expm1')
-
-    def exp2(self):
-        self._check_dimensionless()
-        return self._get_from_numpy('exp2')
-
-    def log(self):
-        self._check_dimensionless()
-        return self._get_from_numpy('log')
-
-    def log10(self):
-        self._check_dimensionless()
-        return self._get_from_numpy('log10')
-
-    def log2(self):
-        self._check_dimensionless()
-        return self._get_from_numpy('log2')
-
-    def log1p(self):
-        self._check_dimensionless()
-        return self._get_from_numpy('log1p')
-
-    # End numpy functions
+    # ------ End pickling support ------
 
     cdef inline tuple unit_as_tuple(self):
         return tuple(self.units())
@@ -524,7 +392,6 @@ cdef class Quantity:
         except:
             return _nou
 
-    # TODO: CHECK WHAT TO DO FOR NP ARRAY
     def __mul__(x, y):
         cdef Quantity xq
         cdef Quantity yq
@@ -538,7 +405,6 @@ cdef class Quantity:
             ans.unit[i] = xq.unit[i] + yq.unit[i]
         return ans
 
-    # TODO: CHECK WHAT TO DO FOR NP ARRAY
     def __div__(x,y):
         cdef Quantity xq = assertQuantity(x)
         cdef Quantity yq = assertQuantity(y)
@@ -548,7 +414,6 @@ cdef class Quantity:
             ans.unit[i] = xq.unit[i] - yq.unit[i]
         return ans
 
-    # TODO: CHECK WHAT TO DO FOR NP ARRAY
     def __truediv__(x, y):
         cdef Quantity xq = assertQuantity(x)
         cdef Quantity yq = assertQuantity(y)
@@ -583,7 +448,6 @@ cdef class Quantity:
         else:
             raise Exception('Impossible.')
 
-    # TODO: CHECK WHAT TO DO FOR NP ARRAY
     def __richcmp__(x, y, int op):
         """
         <   0
@@ -609,9 +473,9 @@ cdef class Quantity:
         elif op == 5:
             return xq.magnitude >= yq.magnitude
 
-    # TODO: CHECK WHAT TO DO FOR NP ARRAY
     def convert(self, Quantity target_unit):
         assert isQuantity(target_unit), 'Target must be a quantity.'
+        assert target_unit.mag_is_array == 0, 'Target must be scalar not an array.'
         sameunits(self, target_unit)
         return self.magnitude / target_unit.magnitude
 
@@ -633,7 +497,6 @@ cdef class Quantity:
         else:
             return ' '.join([number_part, symbol])
 
-    # TODO: CHECK WHAT TO DO FOR NP ARRAY
     def __float__(self):
         assert self.unitCategory() == 'Dimensionless', 'Must be dimensionless for __float__()'
         return self.magnitude
@@ -641,74 +504,14 @@ cdef class Quantity:
     def __rshift__(self, other):
         return self.convert(other)
 
-
-cdef inline int isQuantityNP(var):
-    ''' checks whether var is an instance of type 'Quantity'.
-    Returns True or False.'''
-    return isinstance(var, QuantityNP)
-
-
-cdef inline QuantityNP assertQuantityNP(x):
-    cdef QuantityNP out
-    cdef list a
-    if isinstance(x, QuantityNP):
-        return x
-    elif isinstance(x, Quantity):
-        out = QuantityNP.__new__(QuantityNP, np.array(x.magnitude))
-        a = x.getunit()
-        out.setunit(a)
-        return out
-    else:
-        return QuantityNP.__new__(QuantityNP, np.array(x))
-
-@cython.freelist(8)
-cdef class QuantityNP:
-    cdef readonly np.ndarray magnitude
-    cdef double unit[7]
-    __array_priority__ = 20.0
-
-    def __cinit__(self, np.ndarray magnitude):
-        self.magnitude = magnitude
-        self.unit[:] = [0,0,0,0,0,0,0]
-
-    ##Pickling support
-    def __reduce__(self):
-       return (QuantityNP, (self.magnitude,), self.getunit(), None, None)
-
-    def __setstate__(self, unit):
-        self.setunit(unit)
-
-    #def __array__(self, dtype=None):
-    #    return self.magnitude
-
-    ## We have to disable this for now. ufuncs in danger of giving the wrong
-    ## answer.
-    #def __getattr__(self, name):
-    #    ''' All member lookup not found here get passed to the magnitude value. '''
-    #    return getattr(self.magnitude, name)
-
-    #def __dir__(self):
-    #    return dir(self.magnitude)
-
-    # def __array_wrap__(self, array, context=None):
-    #     print self
-    #     print array
-    #
-    #     cdef QuantityNP ans = assertQuantityNP(array)
-    #     cdef int i
-    #     for i from 0 <= i < 7:
-    #         ans.unit[i] = self.unit[i]
-    #     return ans
-
-
-    # Implement some numpy functions
+    # ------ Implement some numpy functions ------
 
     def _check_dimensionless(self):
             if self.unitCategory() != 'Dimensionless':
                 raise EIncompatibleUnits('Argument must be dimensionless.')
 
     def _get_from_numpy(self, name):
-        cdef QuantityNP out = QuantityNP.__new__(QuantityNP, getattr(np, name)(self.magnitude))
+        cdef Quantity out = Quantity.__new__(Quantity, getattr(np, name)(self.magnitude))
         return out
 
     def sin(self):
@@ -803,277 +606,10 @@ cdef class QuantityNP:
         self._check_dimensionless()
         return self._get_from_numpy('log1p')
 
-    def sqrt(self):
-        cdef QuantityNP ans = QuantityNP.__new__(QuantityNP, np.sqrt(self.magnitude))
-        copyunits(self, ans, 0.5)
-        return ans
-
-    # def var(self, *args, **kwargs):
-    #     print args
-    #     print kwargs
-    #     res = np.var(self.magnitude, *args, **kwargs)
-    #     if isinstance(self.magnitude, np.ndarray):
-    #         cdef QuantityNP ans = QuantityNP.__new__(QuantityNP, np.var(self.magnitude, *args, **kwargs))
-    #     else:
-    #     copyunits(self, ans, 2)
-    #     return ans
-
-    # End numpy functions
-
-    def __getitem__(self, val):
-        cdef int i
-        cdef Quantity ansq
-        if type(val)==int:
-            ansq = Quantity.__new__(Quantity, self.magnitude[val])
-            for i from 0 <= i < 7:
-                ansq.unit[i] = self.unit[i]
-            return ansq
-        cdef QuantityNP ans = QuantityNP.__new__(QuantityNP, self.magnitude[val])
+    def copy(self):
+        cdef Quantity ans = Quantity.__new__(Quantity, self.magnitude)
         copyunits(self, ans, 1)
         return ans
 
-    cdef inline tuple unit_as_tuple(self):
-        return tuple(self.units())
-
-    def setValDict(self, dict valdict):
-        cdef int i
-        cdef list values
-        values = [valdict.get(s) or 0 for s in symbols]
-        for i from 0 <= i < 7:
-            self.unit[i] = values[i]
-
-    def setValDict2(self, **kwargs):
-        cdef int i
-        cdef list values
-        values = [kwargs.get(s) or 0 for s in symbols]
-        for i from 0 <= i < 7:
-            self.unit[i] = values[i]
-
-    def getunit(self):
-        cdef list out
-        cdef int i
-        out = [0.0]*7
-        for i from 0 <= i < 7:
-            out[i] = self.unit[i]
-        return out
-
-    def setunit(self, list unit):
-        cdef int i
-        for i from 0 <= i < 7:
-            self.unit[i] = unit[i]
-
-    def selfPrint(self):
-        dict_contents = ','.join(['{}={}'.format(s,v) for s,v in dict(zip(symbols, self.units())).iteritems() if v != 0.0])
-        return 'Quantity({}, dict({}))'.format(self.magnitude, dict_contents)
-
-    def setRepresent(self, as_unit=None, symbol='',
-        convert_function=None, format_spec='.4g'):
-        '''By default, the target representation is arrived by dividing
-        the current unit MAGNITUDE by the target unit MAGNITUDE, and
-        appending the desired representation symbol.
-
-        However, if a conversion_function is supplied, then INSTEAD the
-        conversion function will be called so:
-
-            output_magnitude = conversion_function(self.magnitude)
-            output_symbol = symbol
-
-            result = '{} {}'.format(output_magnitude, output_symbol)
-
-        The intention of the function argument is to allow
-        non-proportional conversion, typically temperature but also things
-        like barg, bara, etc.
-
-        Note that if a convert_function is supplied, the as_unit arg
-        is IGNORED.'''
-        if not (as_unit or convert_function):
-            raise Exception('Either a target unit or a conversion function must be supplied.')
-
-        if convert_function == None:
-            def proportional_conversion(instance, _):
-                return instance.convert(as_unit)
-            convert_function = proportional_conversion
-        RepresentCache[self.unit_as_tuple()] = dict(
-            convert_function=convert_function,
-            symbol=symbol,
-            format_spec=format_spec)
-
-    def units(self):
-        cdef list out = []
-        cdef int i
-        for i in range(7):
-            out.append(self.unit[i])
-        return out
-
-    def unitString(self):
-        if self.unit_as_tuple() in RepresentCache:
-            r = RepresentCache[self.unit_as_tuple()]
-            ret = '{}'.format(r['symbol'])
-            return ret
-        else:
-            text = ' '.join(['{}^{}'.format(k,v) for k, v in zip(symbols, self.units()) if v != 0])
-            ret = '{}'.format(text)
-            return ret
-
-    def _getmagnitude(self):
-        if self.unit_as_tuple() in RepresentCache:
-            r = RepresentCache[self.unit_as_tuple()]
-            return r['convert_function'](self, self.magnitude)
-        else:
-            return self.magnitude
-
-    def _getsymbol(self):
-        if self.unit_as_tuple() in RepresentCache:
-            r = RepresentCache[self.unit_as_tuple()]
-            return r['symbol']
-        else:
-            return self.unitString()
-
-    def _getRepresentTuple(self):
-        if self.unit_as_tuple() in RepresentCache:
-            r = RepresentCache[self.unit_as_tuple()]
-            mag = r['convert_function'](self, self.magnitude)
-            symbol = r['symbol']
-            format_spec = r['format_spec']
-        else:
-            mag = self.magnitude
-            symbol = self.unitString()
-            format_spec = ''
-        # Temporary fix for a numpy display issue
-        if not type(mag) in [float, int]:
-            format_spec = ''
-        return mag, symbol, format_spec
-
-    def __str__(self):
-        mag, symbol, format_spec = self._getRepresentTuple()
-        number_part = format(mag, format_spec)
-        if symbol == '':
-            return number_part
-        else:
-            return ' '.join([number_part, symbol])
-
-    def __repr__(self):
-        return str(self)
-
-    def __add__(x, y):
-        cdef QuantityNP xq = assertQuantityNP(x)
-        cdef QuantityNP yq = assertQuantityNP(y)
-        sameunits(xq, yq)
-        cdef QuantityNP ans = QuantityNP.__new__(QuantityNP, xq.magnitude + yq.magnitude)
-        cdef int i
-        for i from 0 <= i < 7:
-            ans.unit[i] = xq.unit[i]
-        return ans
-
-    def __sub__(x, y):
-        cdef QuantityNP xq = assertQuantityNP(x)
-        cdef QuantityNP yq = assertQuantityNP(y)
-        sameunits(xq, yq)
-        cdef QuantityNP ans = QuantityNP.__new__(QuantityNP, xq.magnitude - yq.magnitude)
-        cdef int i
-        for i from 0 <= i < 7:
-            ans.unit[i] = xq.unit[i]
-        return ans
-
-    def unpack_or_default(self, other):
-        try:
-            return other.unit
-        except:
-            return _nou
-
-    def __mul__(x, y):
-        cdef QuantityNP xq = assertQuantityNP(x)
-        cdef QuantityNP yq = assertQuantityNP(y)
-        cdef QuantityNP ans = QuantityNP.__new__(QuantityNP, xq.magnitude * yq.magnitude)
-        cdef int i
-        for i from 0 <= i < 7:
-            ans.unit[i] = xq.unit[i] + yq.unit[i]
-        return ans
-
-    def __div__(x,y):
-        cdef QuantityNP xq = assertQuantityNP(x)
-        cdef QuantityNP yq = assertQuantityNP(y)
-        cdef QuantityNP ans = QuantityNP.__new__(QuantityNP, xq.magnitude / yq.magnitude)
-        cdef int i
-        for i from 0 <= i < 7:
-            ans.unit[i] = xq.unit[i] - yq.unit[i]
-        return ans
-
-    def __truediv__(x, y):
-        cdef QuantityNP xq = assertQuantityNP(x)
-        cdef QuantityNP yq = assertQuantityNP(y)
-        cdef QuantityNP ans = QuantityNP.__new__(QuantityNP, xq.magnitude / yq.magnitude)
-        cdef int i
-        for i from 0 <= i < 7:
-            ans.unit[i] = xq.unit[i] - yq.unit[i]
-        return ans
-
-    def __pow__(x, y, z):
-        cdef QuantityNP xq = assertQuantityNP(x)
-        assert not isQuantityNP(y), 'The exponent must not be a quantity!'
-        cdef QuantityNP ans = QuantityNP.__new__(QuantityNP, xq.magnitude ** y)
-        cdef int i
-        for i from 0 <= i < 7:
-            ans.unit[i] = xq.unit[i] * y
-        return ans
-
-    def __neg__(self):
-        cdef QuantityNP ans = QuantityNP.__new__(QuantityNP, -self.magnitude)
-        cdef int i
-        for i from 0 <= i < 7:
-            ans.unit[i] = self.unit[i]
-        return ans
-
-    def __cmp__(x, y):
-        cdef QuantityNP xq = assertQuantityNP(x)
-        cdef QuantityNP yq = assertQuantityNP(y)
-        sameunits(xq, yq)
-        if xq.magnitude < yq.magnitude:
-            return -1
-        elif xq.magnitude == yq.magnitude:
-            return 0
-        elif xq.magnitude > yq.magnitude:
-            return 1
-        else:
-            raise Exception('Impossible.')
-
-    def convert(self, Quantity target_unit):
-        assert isQuantity(target_unit), 'Target must be a quantity.'
-        # Because of how fused types work, I have to manufacture
-        # the comparison
-        sameunitsp(self.unit, target_unit.unit)
-        return self.magnitude / target_unit.magnitude
-
-    def unitCategory(self):
-        if self.unit_as_tuple() in QuantityType:
-            return QuantityType[self.unit_as_tuple()]
-        else:
-            msg = 'The collection of units: "{}" has not been defined as a category yet.'
-            raise Exception(msg.format(str(self)))
-
-    def __format__(self, format_spec):
-        # Ignore the stored format_spec, use the given one.
-        mag, symbol, stored_format_spec = self._getRepresentTuple()
-        if format_spec == '':
-            format_spec = stored_format_spec
-        number_part = format(mag, format_spec)
-        if symbol == '':
-            return number_part
-        else:
-            return ' '.join([number_part, symbol])
-
-    def __float__(self):
-        assert self.unitCategory() == 'Dimensionless', 'Must be dimensionless for __float__()'
-        return self.magnitude
-
-    def __rshift__(self, other):
-        return self.convert(other)
-
-    def copy(self):
-        cdef QuantityNP ans = QuantityNP.__new__(QuantityNP, self.magnitude)
-        cdef int i
-        for i from 0 <= i < 7:
-            ans.unit[i] = self.unit[i]
-        return ans
 
 
